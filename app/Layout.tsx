bce4debf4fa022fb7419702cb9c503e150354cc6
nedim begic <nedim.begic93@gmail.com>
2024-02-22 11:35:43 +0100

    working on header

--- app/Layout.tsx
+++ app/Layout.tsx
@@ -1,5 +1,5 @@
-// components/Layout.tsx
-import React, { ReactNode } from "react";
+import React, { ReactNode, useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "@/mainComponents/Header";
 
 type LayoutProps = {
@@ -7,6 +7,11 @@ type LayoutProps = {
 };
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
+  useEffect(() => {
+    // Import Bootstrap JavaScript only on the client-side
+    import("bootstrap");
+  }, []);
+
   return (
     <div>
       <Header />
--- mainComponents/Header.tsx
+++ mainComponents/Header.tsx
@@ -1,7 +1,51 @@
-export default function Header() {
+import React from "react";
+import style from "./Header.module.css";
+
+const Header: React.FC = () => {
   return (
-    <main>
-      <h1>Header</h1>
-    </main>
+    <nav className={`navbar navbar-expand-lg navbar-dark ${style.nav}`}>
+      <div className="container">
+        <a className="navbar-brand" href="#home">
+          GameShop
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className={`navbar-nav ml-auto ${style.myNav}`}>
+            <li className="nav-item">
+              <a className="nav-link" href="#home">
+                News
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#link">
+                Games
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#about">
+                Add Product
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#about">
+                My Profile
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   );
-}
+};
+
+export default Header;
