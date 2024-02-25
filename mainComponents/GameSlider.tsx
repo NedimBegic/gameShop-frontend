14936fe0a52d8bb8a09affa12acd68d7a669731f
nedim begic <nedim.begic93@gmail.com>
2024-02-25 18:29:56 +0100

    fixing leack of api key

--- mainComponents/GameSlider.tsx
+++ mainComponents/GameSlider.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
-import GameCard, { GameCardProps } from "../sideComponents/GameCard"; // Assuming GameCard component is defined in a separate file
+import GameCard, { GameCardProps } from "../sideComponents/GameCard";
+import style from "./GameSlider.module.css";
 
 const GameSlider: React.FC = () => {
   const [games, setGames] = useState<GameCardProps[]>([]);
@@ -11,8 +12,7 @@ const GameSlider: React.FC = () => {
       const options = {
         method: "GET",
         headers: {
-          "X-RapidAPI-Key":
-            "1e6516cebcmsh52887f1a36ded0bp12b703jsn0ba12181f6a2",
+          "X-RapidAPI-Key": `${process.env.GAMES_API}`,
           "X-RapidAPI-Host": "free-to-play-games-database.p.rapidapi.com",
         },
       };
@@ -23,6 +23,7 @@ const GameSlider: React.FC = () => {
           throw new Error("Failed to fetch games");
         }
         const data = await response.json();
+        console.log(data);
         const gamesData: GameCardProps[] = data.map((game: any) => ({
           title: game.title,
           thumbnail: game.thumbnail,
@@ -43,33 +44,45 @@ const GameSlider: React.FC = () => {
   }, []);
 
   return (
-    <div id="gameSlider" className="carousel slide" data-ride="carousel">
-      <div className="carousel-inner">
+    <div
+      id="gameSlider"
+      className={`carousel slide ${style.gameSlider}`}
+      data-ride="carousel"
+    >
+      <div className={`carousel-inner ${style.carouselInner}`}>
         {games.map((game, index) => (
           <div
             key={index}
-            className={`carousel-item ${index === 0 ? "active" : ""}`}
+            className={`carousel-item ${index === 0 ? "active" : ""} ${
+              style.carouselItem
+            }`}
           >
             <GameCard {...game} />
           </div>
         ))}
       </div>
       <a
-        className="carousel-control-prev"
+        className={`carousel-control-prev ${style.carouselControlPrev}`}
         href="#gameSlider"
         role="button"
         data-slide="prev"
       >
-        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span
+          className={`carousel-control-prev-icon ${style.carouselControlIcon}`}
+          aria-hidden="true"
+        ></span>
         <span className="sr-only">Previous</span>
       </a>
       <a
-        className="carousel-control-next"
+        className={`carousel-control-next ${style.carouselControlNext}`}
         href="#gameSlider"
         role="button"
         data-slide="next"
       >
-        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+        <span
+          className={`carousel-control-next-icon ${style.carouselControlIcon}`}
+          aria-hidden="true"
+        ></span>
         <span className="sr-only">Next</span>
       </a>
     </div>
