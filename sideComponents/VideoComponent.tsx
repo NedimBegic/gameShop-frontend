02cdc89ba846598b9aa65ebe54b532136daa652a
nedim begic <nedim.begic93@gmail.com>
2024-02-28 13:04:45 +0100

    new video component

--- sideComponents/VideoComponent.tsx
+++ sideComponents/VideoComponent.tsx
@@ -1,16 +1,20 @@
 import React from "react";
 
 interface VideoProps {
-  src: string;
+  src: string[];
   width?: number;
   height?: number;
 }
 
 const Video: React.FC<VideoProps> = ({ src, width = 640, height = 360 }) => {
+  // Randomly select one of the video sources
+  const randomIndex = Math.floor(Math.random() * src.length);
+  const randomSrc = src[randomIndex];
+
   return (
     <div>
       <video width={width} height={height} controls>
-        <source src={src} type="video/mp4" />
+        <source src={randomSrc} type="video/mp4" />
         Your browser does not support the video tag.
       </video>
     </div>
